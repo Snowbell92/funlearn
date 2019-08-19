d025c501b462004e6d196b647e55e6e34e2f24f3
Samia Ruponti <samia@bongobd.com>
2019-08-19 20:23:11 +0600

    controller - added onSave function. onSave function in progress.

--- controller/addNewItemController.py
+++ controller/addNewItemController.py
@@ -1,5 +1,7 @@
 from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
 from lib.getConfig import load_config
+import pathlib
+import shutil
 
 
 class AddNewItemController(QObject):
@@ -8,6 +10,8 @@ class AddNewItemController(QObject):
         super().__init__()
         self._model = model
         self.fileList = self._model.fname
+        self.configs = load_config(self, 'settings/config.yml')
+        self.assetDir = self.configs['imagespath']
 
     @pyqtSlot(str)
     def update_objName(self, value):
@@ -17,6 +21,18 @@ class AddNewItemController(QObject):
     def update_filename(self, value):
         self._model.fname.extend(value)
 
+    def unique(self,data):
+        return list(dict.fromkeys(data))
+
     def onSave(self):
-        print(self._model.myObjectName)
-        print(self.fileList)
+        # prints to be removed
+        # print(self._model.myObjectName)
+        # print(self.fileList)
+        # create a folder in the assets dir with the name of the object
+        objectPath = self.assetDir + self._model.myObjectName
+        pathlib.Path(objectPath).mkdir(exist_ok=True)
+
+        # copy the selected files there and rename them
+        fileList = self.unique(self._model.fname)
+        for i in fileList:
+            shutil.copy(i, objectPath)

7cb73a3dbbf103be3a477f54550fc3ac6c48b760
Samia Ruponti <samia@bongobd.com>
2019-08-21 15:55:14 +0600

    updated gitignore. addNewItemController in progress.

--- controller/addNewItemController.py
+++ controller/addNewItemController.py
@@ -21,13 +21,19 @@ class AddNewItemController(QObject):
     def update_filename(self, value):
         self._model.fname.extend(value)
 
-    def unique(self,data):
+    def unique(self, data):
         return list(dict.fromkeys(data))
 
+    def findFiles(self, directory):
+        fileList =[]
+        iterator = directory.iterdir()
+        for entry in iterator:
+            if entry.is_file():
+                fileList.append(entry.name)
+
+        return fileList
+
     def onSave(self):
-        # prints to be removed
-        # print(self._model.myObjectName)
-        # print(self.fileList)
         # create a folder in the assets dir with the name of the object
         objectPath = self.assetDir + self._model.myObjectName
         pathlib.Path(objectPath).mkdir(exist_ok=True)
@@ -36,3 +42,15 @@ class AddNewItemController(QObject):
         fileList = self.unique(self._model.fname)
         for i in fileList:
             shutil.copy(i, objectPath)
+
+        # get new image path after copy
+        imgList = self.findFiles(pathlib.Path(objectPath))
+
+        # rename the copied files
+        for f in imgList:
+            img = pathlib.Path(f)
+            imgExt = img.suffix
+            index = str(imgList.index(f))
+            imgName = "-".join([self._model.myObjectName, index])
+            imgPath = pathlib.Path(objectPath)+img
+            imgPath.rename(imgName+imgExt)

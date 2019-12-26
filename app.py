2ee90f167b5410c950e552d02eb2bbe8ed5634db
Samia Ruponti <samia@bongobd.com>
2019-12-26 12:28:39 +0600

    commit unused files- because I'm deleiting from local.

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-#from models.addNewItemModel import ModelAddNewItem
-#from views.additem import AddNewItem
-#from controller.addNewItemController import AddNewItemController
-from views.showItem import ShowItem
-from controller.showItemController import ShowItemController
+from models.addNewItemModel import ModelAddNewItem
+from views.additem import AddNewItem
+from controller.addNewItemController import AddNewItemController
+#from views.showItem import ShowItem
+#from controller.showItemController import ShowItemController
 
 
 class App(QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
-        #self.model = ModelAddNewItem()
-        self.main_controller = ShowItemController()
-        self.main_view = ShowItem(self.main_controller)
+        self.model = ModelAddNewItem()
+        self.main_controller = AddNewItemController(self.model)
+        self.main_view = AddNewItem(self.model, self.main_controller)
         self.main_view.show()
 
 
